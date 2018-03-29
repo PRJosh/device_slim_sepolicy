91fcfd7cc0522703b4512345bf22f8fabe998895
Josue Rivera <prbassplayer@gmail.com>
2018-03-29 22:32:43 +0200

    Revert "sepolicy: Isolate qcom sysfs_graphics policy for recovery"
    This reverts commit c4c96f6145838adb949b204ea7e3828ec2eb72a7.
    
    Conflicts:
    	qcom/private/recovery.te
    	qcom/sepolicy.mk
    
    Change-Id: I87e04c6ccafa1b71fa99b6edce9f1a2057997047

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -3,9 +3,6 @@
 # that inherit from Lineage
 #
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/slim/sepolicy/qcom/private
-
 BOARD_SEPOLICY_DIRS += \
     device/slim/sepolicy/qcom/common \
     device/slim/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
